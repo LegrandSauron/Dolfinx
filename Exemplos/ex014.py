9b255eef376b3843b29b0f057ac8d5f301015cb7
vinicius <viniciuspaes777@gmail.com>
2023-09-02 15:06:18 -0300

    updates

--- Exemplos/ex014.py
+++ Exemplos/ex014.py
@@ -1,71 +1,142 @@
-from dolfinx import fem
-import ufl
-from petsc4py.PETSc import ScalarType
-from dolfinx import fem,log, nls ,mesh
-from dolfinx.io import gmshio
-from mpi4py import MPI
 import numpy as np
+import ufl
+
 from petsc4py import PETSc
+from mpi4py import MPI
+from dolfinx import fem, mesh, plot
+from dolfinx.io import gmshio
 
-# Scaled variable
-carregamento= 1 #-5000
-E = 1# 78e6
-poisson = 1# 0.3
-lambda_ =  1 #E*poisson / ((1+poisson)*(1-2*poisson))
-G= 1#E / 2*(1+poisson)
+domain, cell_tags, facet_tags = gmshio.read_from_msh("t11_gmsh_malha.msh", MPI.COMM_SELF,0, gdim=2)
+
+V = fem.VectorFunctionSpace(domain, ("CG", 2))
 
 
-
-domain = mesh.create_rectangle(comm=MPI.COMM_WORLD,
-                            points=((0.0, 0.0), (2.0, 1.0)), n=(300, 300),
-                            cell_type=mesh.CellType.triangle,)
-
-V = fem.VectorFunctionSpace(domain, ("CG", 1))
-
-def clamped_boundary(x):
-    return np.isclose(x[0], 0)
-
+"""Condições de contorno"""
+u_bc = np.array((0,) * domain.geometry.dim, dtype=PETSc.ScalarType)
 fdim = domain.topology.dim - 1
-boundary_facets = mesh.locate_entities_boundary(domain, fdim, clamped_boundary)
+dofs_2 = fem.locate_dofs_topological(V, facet_tags.dim, facet_tags.find(101))
+bcs=[fem.dirichletbc(u_bc,dofs=dofs_2,V=V)]
 
-u_D = np.array([0,0], dtype=ScalarType)
-bc = fem.dirichletbc(u_D, fem.locate_dofs_topological(V, fdim, boundary_facets), V)
+"""fim"""
 
-#Aproximação das funções teste e funcao incognita
+B = fem.Constant(domain, PETSc.ScalarType((0, 0)))
+T = fem.Constant(domain, PETSc.ScalarType((0, 0)))
+
+
+
 v = ufl.TestFunction(V)
-u= fem.Function(V)
-uh = ufl.TrialFunction(V)
+u = fem.Function(V)
 
-#hencky's strain tensor
+
+# Spatial dimension
 d = len(u)
-I = (ufl.Identity(d)) 
-F =(I + ufl.grad(u)) 
 
-#tensor de cauchy-Green left 
-C= (F * F.T )
+# Identity tensor
+I = ufl.variable(ufl.Identity(d))
 
-#determinando o Jacobiano
-Jhc_e= ufl.det(F)
-J= Jhc_e
+# Deformation gradient
+F = ufl.variable(I + ufl.grad(u))
 
+# Right Cauchy-Green tensor
+C = ufl.variable(F * F.T)
+
+# Invariants of deformation tensors
+Ic = ufl.variable(ufl.tr(C))
+J  = ufl.variable(ufl.det(F))
+
+
+
+# Elasticity parameters
+E = PETSc.ScalarType(1.0e4)
+nu = PETSc.ScalarType(0.3)
+mu = fem.Constant(domain, E/(2*(1 + nu)))
+lmbda = fem.Constant(domain, E*nu/((1 + nu)*(1 - 2*nu)))
+
+
+# Stored strain energy density (compressible neo-Hookean model)
+psi = (mu / 2) * (Ic - 3) - mu * ufl.ln(J) + (lmbda / 2) * (ufl.ln(J))**2
+
+# Stress
+# Hyper-elasticity
+P = ufl.diff(psi, F)
+P = 2.0 * mu * ufl.sym(ufl.grad(u)) + lmbda * ufl.tr(ufl.sym(ufl.grad(u))) * I
+
+"""Tensor de hencky"""
 #Hencky's Strain
-N, M = C.ufl_shape
-T_hencky=ufl.as_tensor([[(1/2)*(ufl.ln(C[i,j])) for i in range(N)] for j in range(M)])
+#N, M = C.ufl_shape
+#T_hencky=ufl.as_tensor([[(1/2)*(ufl.ln(C[i,j])) for i in range(N)] for j in range(M)])
 
-
-#Tensor de tensões
-def T_tension(u):
-    oo = len(u)
-    o = (ufl.Identity(oo)) 
-    return (1/J)*(2.0 * G * T_hencky + lambda_ * ufl.tr(T_hencky) * o )
-
-
-#Funcao carregamento = f(0,y)
-f = fem.Constant(domain,ScalarType((0,carregamento)))
-
-#Formulação variacional para Div T = 0, em Tn= f,u= 0 em x=0
-#Weak form for  Div T = 0,  Tn= f, u= 0 in 
-F_bilinear = ufl.inner(ufl.grad(v),T_tension)*ufl.dx - ufl.inner(f, v)*ds
+"""Tensor de tensoes"""
+#P= (1/J)*(2.0 * mu * T_hencky + lmbda * ufl.tr(T_hencky) * I )
 
 
 
+metadata = {"quadrature_degree": 4}
+ds = ufl.Measure('ds', domain=domain, subdomain_data=facet_tags, metadata=metadata)
+dx = ufl.Measure("dx", domain=domain, metadata=metadata)
+
+# Define form F (we want to find u such that F(u) = 0)
+F = ufl.inner(ufl.grad(v), P)*dx - ufl.inner(v, B)*dx - ufl.inner(v, T)*ds(102) 
+
+
+problem = fem.petsc.NonlinearProblem(F, u, bcs)
+
+
+from dolfinx import nls
+solver = nls.petsc.NewtonSolver(domain.comm, problem)
+
+# Set Newton solver options
+solver.atol = 1e-8
+solver.rtol = 1e-8
+solver.convergence_criterion = "incremental"
+
+
+
+import pyvista
+import matplotlib.pyplot as plt
+pyvista.start_xvfb()
+plotter = pyvista.Plotter()
+plotter.open_gif("deformation.gif", fps=3)
+
+topology, cells, geometry = plot.create_vtk_mesh(u.function_space)
+function_grid = pyvista.UnstructuredGrid(topology, cells, geometry)
+
+values = np.zeros((geometry.shape[0], 2))
+values[:, :len(u)] = u.x.array.reshape(geometry.shape[0], len(u))
+function_grid["u"] = values
+function_grid.set_active_vectors("u")
+
+# Warp mesh by deformation
+warped = function_grid.warp_by_vector("u", factor=0)
+warped.set_active_vectors("u")
+
+# Add mesh to plotter and visualize
+actor = plotter.add_mesh(warped, show_edges=True, lighting=False, clim=[0, 10])
+
+# Compute magnitude of displacement to visualize in GIF
+Vs = fem.FunctionSpace(domain, ("Lagrange", 2))
+magnitude = fem.Function(Vs)
+us = fem.Expression(ufl.sqrt(sum([u[i]**2 for i in range(len(u))])), Vs.element.interpolation_points())
+magnitude.interpolate(us)
+warped["mag"] = magnitude.x.array
+
+
+
+from dolfinx import log
+log.set_log_level(log.LogLevel.INFO)
+tval0 = -1.5
+for n in range(1, 10):
+    T.value[1] = n * tval0
+    num_its, converged = solver.solve(u)
+    assert(converged)
+    u.x.scatter_forward()
+    print(f"Time step {n}, Number of iterations {num_its}, Load {T.value}")
+    function_grid["u"][:, :len(u)] = u.x.array.reshape(geometry.shape[0], len(u))
+    magnitude.interpolate(us)
+    warped.set_active_scalars("mag")
+    warped_n = function_grid.warp_by_vector(factor=1)
+    plotter.update_coordinates(warped_n.points.copy(), render=False)
+    plotter.update_scalar_bar_range([0, 10])
+    plotter.update_scalars(magnitude.x.array)
+    plotter.write_frame()
+plotter.close()
